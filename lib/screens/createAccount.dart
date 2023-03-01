cfe5a2e2bb40b9aeee7124ce16f50fbc286b41c6
mkc2001 <millerkclark@gmail.com>
2023-03-01 00:23:20 -0700

    removed referenced to the app page

--- lib/screens/createAccount.dart
+++ lib/screens/createAccount.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:serve_to_be_free/utilities/constants.dart';
-import 'package:serve_to_be_free/screens/app.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
