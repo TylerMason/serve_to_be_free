1fef8753bdbd93d1ce704d3f03e0a747e91d08b9
mkc2001 <millerkclark@gmail.com>
2023-04-01 13:29:35 -0600

    fixed bug where banner on project page had error

--- lib/screens/projects.dart
+++ lib/screens/projects.dart
@@ -76,8 +76,9 @@ class _ProjectsPageState extends State<ProjectsPage> {
   Widget build(BuildContext context) {
     precacheImage(AssetImage("assets/images/19219.jpg"), context);
     return Scaffold(
+        resizeToAvoidBottomInset: false,
         appBar: PreferredSize(
-          preferredSize: Size.fromHeight(150.0),
+          preferredSize: Size.fromHeight(192.0),
           child: AppBar(
             flexibleSpace: Container(
               decoration: BoxDecoration(
@@ -91,113 +92,118 @@ class _ProjectsPageState extends State<ProjectsPage> {
                 ),
               ),
               padding: EdgeInsets.only(top: 60),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Container(
-                    width: 280,
-                    height: 60,
-                    decoration: BoxDecoration(
-                      image: DecorationImage(
-                        image: AssetImage(
-                            'assets/images/STBF_logo_horizontal_navy.jpg'),
-                        //fit: BoxFit.cover,
+              // padding: EdgeInsets.only(bottom: 10)
+              child: SingleChildScrollView(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Container(
+                      width: 280,
+                      height: 100,
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                          image: AssetImage(
+                              'assets/images/STBF_logo_horizontal_navy.jpg'),
+                          //fit: BoxFit.cover,
+                        ),
                       ),
                     ),
-                  ),
-                  Container(height: 8),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [
-                      ProjectAppbarDisplay(subject: "Members", value: "9,812"),
-                      ProjectAppbarDisplay(subject: "Projects", value: "287"),
-                      ProjectAppbarDisplay(subject: "Hours", value: "10,400")
-                    ],
-                  )
-                ],
+                    Container(height: 8),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        ProjectAppbarDisplay(
+                            subject: "Members", value: "9,812"),
+                        ProjectAppbarDisplay(subject: "Projects", value: "287"),
+                        ProjectAppbarDisplay(subject: "Hours", value: "10,400"),
+                      ],
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
         ),
         body: SingleChildScrollView(
             child: Container(
-          margin: const EdgeInsets.only(top: 20.0),
-          child: Column(
-            children: [
-              wideBorderButton(
-                  "Find a Project",
-                  Icon(
-                    Icons.search,
-                    color: Colors.indigo[900],
-                    size: 28.0,
+                margin: const EdgeInsets.only(top: 20.0),
+                child: Expanded(
+                  child: Column(
+                    children: [
+                      wideBorderButton(
+                          "Find a Project",
+                          Icon(
+                            Icons.search,
+                            color: Colors.indigo[900],
+                            size: 28.0,
+                          ),
+                          widget.findProjectsPath),
+                      wideBorderButton(
+                          "Create a Project",
+                          Icon(
+                            Icons.add_outlined,
+                            color: Colors.blue[600],
+                            size: 28.0,
+                          ),
+                          widget.createProjectsPath),
+                      wideBorderButton(
+                          "Lead a Project",
+                          Icon(
+                            Icons.star_border_rounded,
+                            color: Colors.amberAccent[400],
+                            size: 28.0,
+                          ),
+                          widget.leadProjectsPath),
+                      wideBorderButton(
+                          "Sponsor a Project",
+                          Icon(
+                            Icons.favorite_border_rounded,
+                            color: Colors.pinkAccent[400],
+                            size: 28.0,
+                          ),
+                          widget.sponsorProjectsPath),
+                      SponsorCard(),
+                      Container(
+                        margin: EdgeInsets.only(left: 25, right: 25, top: 30),
+                        child: Row(children: [
+                          Text("My Projects",
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold, fontSize: 20)),
+                          Spacer(),
+                          TextButton(
+                            style: TextButton.styleFrom(
+                              textStyle: const TextStyle(
+                                  fontSize: 18, fontWeight: FontWeight.bold),
+                            ),
+                            onPressed: () {
+                              context.go(widget.myProjectsPath);
+                            },
+                            child: const Text('See all'),
+                          ),
+                        ]),
+                      ),
+                      Container(
+                          margin: EdgeInsets.only(left: 20, right: 20),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [MyProjectCard(), MyProjectCard()],
+                          )),
+                      // ClipRRect(
+                      //   borderRadius: BorderRadius.circular(10),
+                      //   child: Container(
+                      //     width: 200,
+                      //     height: 200,
+                      //     decoration: BoxDecoration(
+                      //       image: DecorationImage(
+                      //         image: AssetImage('path/to/image.png'),
+                      //         fit: BoxFit.cover,
+                      //       ),
+                      //     ),
+                      //   ),
+                      // ),
+                    ],
                   ),
-                  widget.findProjectsPath),
-              wideBorderButton(
-                  "Create a Project",
-                  Icon(
-                    Icons.add_outlined,
-                    color: Colors.blue[600],
-                    size: 28.0,
-                  ),
-                  widget.createProjectsPath),
-              wideBorderButton(
-                  "Lead a Project",
-                  Icon(
-                    Icons.star_border_rounded,
-                    color: Colors.amberAccent[400],
-                    size: 28.0,
-                  ),
-                  widget.leadProjectsPath),
-              wideBorderButton(
-                  "Sponsor a Project",
-                  Icon(
-                    Icons.favorite_border_rounded,
-                    color: Colors.pinkAccent[400],
-                    size: 28.0,
-                  ),
-                  widget.sponsorProjectsPath),
-              SponsorCard(),
-              Container(
-                margin: EdgeInsets.only(left: 25, right: 25, top: 30),
-                child: Row(children: [
-                  Text("My Projects",
-                      style:
-                          TextStyle(fontWeight: FontWeight.bold, fontSize: 20)),
-                  Spacer(),
-                  TextButton(
-                    style: TextButton.styleFrom(
-                      textStyle: const TextStyle(
-                          fontSize: 18, fontWeight: FontWeight.bold),
-                    ),
-                    onPressed: () {
-                      context.go(widget.myProjectsPath);
-                    },
-                    child: const Text('See all'),
-                  ),
-                ]),
-              ),
-              Container(
-                  margin: EdgeInsets.only(left: 20, right: 20),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                    children: [MyProjectCard(), MyProjectCard()],
-                  )),
-              // ClipRRect(
-              //   borderRadius: BorderRadius.circular(10),
-              //   child: Container(
-              //     width: 200,
-              //     height: 200,
-              //     decoration: BoxDecoration(
-              //       image: DecorationImage(
-              //         image: AssetImage('path/to/image.png'),
-              //         fit: BoxFit.cover,
-              //       ),
-              //     ),
-              //   ),
-              // ),
-            ],
-          ),
-        )));
+                ))));
   }
 }
