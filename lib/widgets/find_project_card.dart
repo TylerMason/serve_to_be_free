dcde064b71c3000a7f4abfd52d5e82d9b79013d4
mkc2001 <89720089+mkc2001@users.noreply.github.com>
2023-04-01 00:34:49 -0600

    Revert "project details"

--- lib/widgets/find_project_card.dart
+++ lib/widgets/find_project_card.dart
@@ -1,36 +1,28 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
 
 class ProjectCard extends StatelessWidget {
   final String title;
   final String numMembers;
-  final Map<String, dynamic> projectJson;
   // final String thumbnailUrl;
 
-  ProjectCard(
-      {required this.title, required this.numMembers, required this.projectJson
-      // required this.thumbnailUrl,
-      });
+  ProjectCard({
+    required this.title,
+    required this.numMembers,
+    // required this.thumbnailUrl,
+  });
 
   // Named constructor that accepts a JSON object
   ProjectCard.fromJson(Map<String, dynamic> json)
       : title = json['name'],
-        numMembers = json['members'].length.toString(),
-        projectJson = json;
+        numMembers = json['members'].length.toString();
 
   @override
   Widget build(BuildContext context) {
     return Center(
-        child: Container(
-      width: double.infinity,
-      height: 100.0,
-      child: InkWell(
-        onTap: () {
-          context.go('/projects/projectDetails');
-        },
-        child: new Card(
+      child: Container(
+        width: double.infinity,
+        height: 100.0,
+        child: Card(
           color: Colors.white,
           shadowColor: Colors.grey.withOpacity(0.4),
           elevation: 4.0,
@@ -54,6 +46,6 @@ class ProjectCard extends StatelessWidget {
           ),
         ),
       ),
-    ));
+    );
   }
 }
